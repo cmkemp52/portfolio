ea52effc87dfdee2cf92566c409acae78170a365
Christopher Kemp <cmkemp52@gmail.com>
2019-10-30 10:23:41 -0400

    added base

---
+++ scripts/main.js
@@ -0,0 +1,13 @@
+
+{VANTA.BIRDS({
+    el: "#startDiv",
+    color1: 0x4b4f57,
+    color2: 0xcacaca,
+    colorMode: "lerpGradient",
+    birdSize: 1.30,
+    separation: 40.00,
+    alignment: 30.00,
+    cohesion: 23.00,
+    quantity: 4.00,
+    backgroundAlpha: 0.84
+})}
