85ddea02b9a49ce31e88be39b6404519fa91732b
Christopher Kemp <cmkemp52@gmail.com>
2019-11-03 12:26:07 -0500

    Finished most mobile styling

--- scripts/main.js
+++ scripts/main.js
@@ -14,9 +14,9 @@ VANTA.BIRDS({
 
 //projects tabs
 
-const libraryTab = document.querySelector("#linkLibrary");
-const busTab = document.querySelector("#linkBus");
-const vitaTab = document.querySelector("#linkVita");
+const libraryTab = document.querySelector(".linkLibrary");
+const busTab = document.querySelector(".linkBus");
+const vitaTab = document.querySelector(".linkVita");
 
 const librarySelect = document.querySelector(".libraryDiv");
 const busSelect = document.querySelector(".busDiv");
@@ -26,14 +26,30 @@ libraryTab.addEventListener("click", function(){
     librarySelect.setAttribute("id","libraryShow");
     busSelect.setAttribute("id","projectHide");
     vitaSelect.setAttribute("id","projectHide");
+    libraryTab.setAttribute("id","libraryLinkSelect");
+    busTab.setAttribute("id","busLink");
+    vitaTab.setAttribute("id","vitaLink");
 });
 busTab.addEventListener("click", function(){
     librarySelect.setAttribute("id","projectHide");
     busSelect.setAttribute("id","busShow");
     vitaSelect.setAttribute("id","projectHide");
+    libraryTab.setAttribute("id","libraryLink");
+    busTab.setAttribute("id","busLinkSelect");
+    vitaTab.setAttribute("id","vitaLink");
 });
 vitaTab.addEventListener("click", function(){
     librarySelect.setAttribute("id","projectHide");
     busSelect.setAttribute("id","projectHide");
     vitaSelect.setAttribute("id","vitaShow");
+    libraryTab.setAttribute("id","libraryLink");
+    busTab.setAttribute("id","busLink");
+    vitaTab.setAttribute("id","vitaLinkSelect");
 });
+
+console.log(window.innerWidth);
+
+{if(window.innerWidth > 768){
+    document.querySelector(".busImage").setAttribute("src", "images/bustalker.png")
+    document.querySelector(".vitaImage").setAttribute("src", "images/myvitalog.png")
+}}
